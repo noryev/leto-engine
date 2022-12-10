2078c3cdfc5da7ed78041092d3d6ea579615805a
Logan Lentz <logan.lentz1@gmail.com>
2022-12-10 03:14:14 -0600

    gateway app.get function

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const res = require('express/lib/response')
 const app = express()
 
 const port = 8080
 
+app.get("/", (req, res) => {
+    res.send("yooooo!")
+})
+
 app.listen(port, () => {
     console.log('listening on port ${port}')
 })
+
+// HTTP => POST, GET, PUT/PATCH, 
