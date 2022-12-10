f87e7d0823f8b0d641fd1791e17d1f626e4cadce
Logan Lentz <logan.lentz1@gmail.com>
2022-12-10 03:21:01 -0600

    i like this port more for dev

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const res = require('express/lib/response')
 const app = express()
 
-const port = 8080
+const port = 2222
 
 app.get("/metrics", (req, res) => {
     res.send("bafy...")
