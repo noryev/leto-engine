000ca1ceb1c0687b20e75540b84e399502d477b3
Logan Lentz <logan.lentz1@gmail.com>
2022-12-10 03:29:50 -0600

    using url for api lookup function

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const app = express()
 
 const port = 2222
 
-app.get("/metrics", (req, res) => {
+app.get("/", (req, res) => {
     res.send("bafy...")
 })
 
 // This is the IPFS cid/bafy handler for the anon metrics API
-app.get("/metrics", (req, res) => {
-    res.send("bafy...")
+app.get("/metrics/:cid", (req, res) => {
+    res.send(`What CID do you want to lookup? ${req.params.cid}?
+    `)
 })
+
+// dont do anything with this- its important
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
 })
